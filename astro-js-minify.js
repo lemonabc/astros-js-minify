d757051639df55e9cbbe9430b13a68a061c6b5cf
tiger <wanhu88@gmail.com>
2015-12-02 17:12:54 +0800

    Initial commit

---
+++ astro-js-minify.js
@@ -0,0 +1,27 @@
+var uglifyJs = require('uglify-js');
+
+module.exports = new astro.Middleware({
+    modType:'page',
+    fileType:'js'
+}, function(asset, next) {
+    if (asset.prjCfg.compressJs || astro.evn == 'release') {
+        try {
+            var code = uglifyJs.minify(asset.data, {
+                fromString: true
+            }).code;
+            asset._data = asset.data;
+            asset.data = code;
+        } catch (error) {
+            console.error('astro-js-minify(asset:%s)%s', 
+                asset.info, '语法错误');
+
+            var line = 1;
+            asset.data = '// message:\t' + error.message + '\n'+
+                "// line:\t" + error.line + '\n// input is:\n\n' +
+                asset.data.replace(/(.*)\n?/ig, function(a, b) {
+                    return '/*' + line++ + '*/  ' + b + '\n';
+                });
+        }
+    }
+    next(asset);
+});
---
+++ readme.md
@@ -0,0 +1,5 @@
+# astros-js-minify
+
+发布时，对JS进行压缩和混淆
+
+若要在开发环境开启压缩，在项目配置中加入 `compressJs:true` 即可
